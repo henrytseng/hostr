55042651b8e507f9b8d51d226ff848cb8155cfbf
Henry Tseng <henryiis@gmail.com>
2014-12-02 23:46:10 -0500

    Updated test

--- tests/server.spec.js
+++ tests/server.spec.js
@@ -1,10 +1,17 @@
 'use strict';
 
 describe('Server', function() {
-  it('Should initialize without error', function() {
+  it('Should initialize without error', function(done) {
     var fork = require('child_process').fork;
 
+    var delegate = {
+      handler: function() {
+        server.kill('SIGKILL');
+        done();
+      }
+    };
+
     var server = fork('lib/server.js');
-    server.kill('SIGKILL');
+    server.on('message', delegate.handler);
   });
 });
